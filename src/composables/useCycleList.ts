c8210e0c4ad7e8ddb8ccbddc72fa2bdece269624
Daniel Kelly <me@danielkelly.io>
2024-08-22 13:37:43 -0500

    4-return-data-and-functions-from-composables

--- src/composables/useCycleList.ts
+++ src/composables/useCycleList.ts
@@ -1,3 +1,10 @@
+import { ref } from 'vue'
+
 export const useCycleList = (list: any[]) => {
-  
-};
+  return {
+    state: ref(''),
+    prev: () => {},
+    next: () => {},
+    go: () => {}
+  }
+}
